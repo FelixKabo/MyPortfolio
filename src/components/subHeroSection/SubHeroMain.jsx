9f84f78e36df6c0e9b77e562d6a9a6ef40573638
FelixKabo <137159313+FelixKabo@users.noreply.github.com>
2025-09-29 17:39:00 +0700

    Save local changes

--- src/components/subHeroSection/SubHeroMain.jsx
+++ src/components/subHeroSection/SubHeroMain.jsx
@@ -1,9 +1,9 @@
 const SubHeroMain = () => {
   return (
-    <div className="w-full border-y bg-brown border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 md:flex-row sm:flex-col items-center gap-4">
+    <div className="flex items-center justify-around w-full gap-4 py-8 uppercase border-y bg-brown border-lightGrey text-lightGrey xl:text-4xl md:text-2xl sm:text-4xl md:flex-row sm:flex-col">
       <p className="md:block sm:hidden">Fast Learner</p>
       <p className="md:block sm:hidden">Team Work</p>
-      <p>Details Master</p>
+      <p className="md:block sm:hidden">Detail Oriented</p>
     </div>
   );
 };
